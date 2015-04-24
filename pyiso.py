7dc19a79c2d2812de99af2c7d58b02efff28c8a5
Chris Lalancette <clalancette@gmail.com>
2015-04-24 12:54:15 -0400

    Add in more parts of the PVD.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- pyiso.py
+++ pyiso.py
@@ -1,5 +1,14 @@
 import struct
-import binascii
+
+# There are a number of specific ways that numerical data is stored in the
+# ISO9660/Ecma-119 standard.  In the text these are reference by the section
+# number they are stored in.  A brief synopsis:
+#
+# 7.1.1 - 8-bit number
+# 7.2.3 - 16-bit number, stored first as little-endian then as big-endian (4 bytes total)
+# 7.3.1 - 32-bit number, stored as little-endian
+# 7.3.2 - 32-bit number ,stored as big-endian
+# 7.3.3 - 32-bit number, stored first as little-endian then as big-endian (8 bytes total)
 
 class PyIso(object):
     VOLUME_DESCRIPTOR_TYPE_PRIMARY = 1
@@ -9,8 +18,14 @@ class PyIso(object):
         """
         # check out the primary volume descriptor to make sure it is sane
         cdfd.seek(16*2048)
-        fmt = "=B5sBB32s32sQLLQQQQHHHHHHLL"
-        (desc_type, identifier, version, unused1, system_identifier, volume_identifier, unused2, space_size_le, space_size_be, unused3dot1, unused3dot2, unused3dot3, unused3dot4, set_size_le, set_size_be, seqnum_le, seqnum_be, lb_size_le, lb_size_be, path_table_size_le, path_table_size_be) = struct.unpack(fmt, cdfd.read(struct.calcsize(fmt)))
+        fmt = "=B5sBB32s32sQLLQQQQHHHHHHLLLLLL34s"
+        (desc_type, identifier, version, unused1, system_identifier,
+         volume_identifier, unused2, space_size_le, space_size_be, unused3dot1,
+         unused3dot2, unused3dot3, unused3dot4, set_size_le, set_size_be,
+         seqnum_le, seqnum_be, lb_size_le, lb_size_be, path_table_size_le,
+         path_table_size_be, path_table_loc_le, optional_path_table_loc_le,
+         path_table_loc_be, optional_path_table_loc_be,
+         root_dir_record) = struct.unpack(fmt, cdfd.read(struct.calcsize(fmt)))
 
         # According to Ecma-119, 8.4.1, the primary volume descriptor type should be 1
         if desc_type != self.VOLUME_DESCRIPTOR_TYPE_PRIMARY:
@@ -46,6 +61,12 @@ class PyIso(object):
         print("'%s'" % lb_size_be)
         print("'%s'" % path_table_size_le)
         print("'%s'" % path_table_size_be)
+        print("'%s'" % path_table_loc_le)
+        print("'%s'" % optional_path_table_loc_le)
+        print("'%s'" % path_table_loc_be)
+        print("'%s'" % optional_path_table_loc_be)
+        # FIXME: the root directory record needs to be implemented correctly;
+        # right now we just have it as a 34-byte string placeholder
 
         #return self._PrimaryVolumeDescriptor(version, system_identifier,
         #                                     volume_identifier, space_size_le,
