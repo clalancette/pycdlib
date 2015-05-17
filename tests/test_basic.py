b2620050f7467032317803987e133a4473c1c3d6
Chris Lalancette <clalancette@gmail.com>
2015-05-17 19:37:04 -0400

    Add more tests.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -68,7 +68,7 @@ def check_common_dotdot_dir_record(dotdot_record):
 
 def test_parse_nofiles(tmpdir):
     # First set things up, and generate the ISO with genisoimage.
-    outfile = tmpdir.join("no-file-test.iso")
+    outfile = tmpdir.join("nofile-test.iso")
     indir = tmpdir.mkdir("nofile")
     subprocess.call(["genisoimage", "-v", "-v", "-iso-level", "1", "-no-pad",
                      "-o", str(outfile), str(indir)])
@@ -103,7 +103,7 @@ def test_parse_nofiles(tmpdir):
 
 def test_parse_onefile(tmpdir):
     # First set things up, and generate the ISO with genisoimage.
-    outfile = tmpdir.join("one-file-test.iso")
+    outfile = tmpdir.join("onefile-test.iso")
     indir = tmpdir.mkdir("onefile")
     outfp = open(os.path.join(str(tmpdir), "onefile", "foo"), 'wb')
     outfp.write("foo\n")
@@ -159,12 +159,12 @@ def test_parse_onefile(tmpdir):
 
 def test_parse_twofile(tmpdir):
     # First set things up, and generate the ISO with genisoimage.
-    outfile = tmpdir.join("one-file-test.iso")
-    indir = tmpdir.mkdir("onefile")
-    outfp = open(os.path.join(str(tmpdir), "onefile", "foo"), 'wb')
+    outfile = tmpdir.join("twofile-test.iso")
+    indir = tmpdir.mkdir("twofile")
+    outfp = open(os.path.join(str(tmpdir), "twofile", "foo"), 'wb')
     outfp.write("foo\n")
     outfp.close()
-    outfp = open(os.path.join(str(tmpdir), "onefile", "bar"), 'wb')
+    outfp = open(os.path.join(str(tmpdir), "twofile", "bar"), 'wb')
     outfp.write("bar\n")
     outfp.close()
     subprocess.call(["genisoimage", "-v", "-v", "-iso-level", "1", "-no-pad",
@@ -179,14 +179,14 @@ def test_parse_twofile(tmpdir):
     # With two files, the ISO should be exactly 26 extents long (24 extents for
     # all of the metadata, then 1 extent for each of the two short files).
     assert(iso.pvd.space_size == 26)
-    # With one file, the path table should be exactly 10 bytes (just for the
+    # With two files, the path table should be exactly 10 bytes (just for the
     # root directory entry).
     assert(iso.pvd.path_tbl_size == 10)
 
     # Now check the root directory record.
     check_common_root_dir_record(iso.pvd.root_dir_record)
-    # With one file at the root, the root directory record should have children
-    # of the "dot" record, the "dotdot" record, and the file.
+    # With two files at the root, the root directory record should have children
+    # of the "dot" record, the "dotdot" record, and the two files.
     assert(len(iso.pvd.root_dir_record.children) == 4)
 
     # Now check the "dot" directory record.
@@ -226,3 +226,68 @@ def test_parse_twofile(tmpdir):
     out = StringIO.StringIO()
     iso.get_and_write("/bar", out)
     assert(out.getvalue() == "bar\n")
+
+def test_parse_onefile_onedir(tmpdir):
+    # First set things up, and generate the ISO with genisoimage.
+    outfile = tmpdir.join("onefileonedir-test.iso")
+    indir = tmpdir.mkdir("onefileonedir")
+    outfp = open(os.path.join(str(tmpdir), "onefileonedir", "foo"), 'wb')
+    outfp.write("foo\n")
+    outfp.close()
+    tmpdir.mkdir("onefileonedir/dir1")
+    subprocess.call(["genisoimage", "-v", "-v", "-iso-level", "1", "-no-pad",
+                     "-o", str(outfile), str(indir)])
+
+    # Now open up the ISO with pyiso and check some things out.
+    iso = pyiso.PyIso()
+    iso.open(open(str(outfile), 'rb'))
+
+    # Do checks on the PVD.
+    check_common_pvd(iso.pvd)
+    # With one file and one directory, the ISO should be exactly 26 extents
+    # long (24 extents for all of the metadata, then 1 extent for the file, and
+    # 1 extent for the extra directory.
+    assert(iso.pvd.space_size == 26)
+    # With one file and one directory, the path table should be exactly 22
+    # bytes (10 for the root directory entry, and 12 for the "dir1" entry).
+    assert(iso.pvd.path_tbl_size == 22)
+
+    # Now check the root directory record.
+    check_common_root_dir_record(iso.pvd.root_dir_record)
+    # With two files at the root, the root directory record should have children
+    # of the "dot" record, the "dotdot" record, and the two files.
+    assert(len(iso.pvd.root_dir_record.children) == 4)
+
+    # Now check the "dot" directory record.
+    check_common_dot_dir_record(iso.pvd.root_dir_record.children[0])
+
+    # Now check the "dotdot" directory record.
+    check_common_dotdot_dir_record(iso.pvd.root_dir_record.children[1])
+
+    # The "dir1" directory should have two children (the "dot" and the "dotdot"
+    # entries).
+    assert(len(iso.pvd.root_dir_record.children[2].children) == 2)
+    # The "dir1" directory should be a directory.
+    assert(iso.pvd.root_dir_record.children[2].isdir == True)
+    # The "dir1" directory should not be the root.
+    assert(iso.pvd.root_dir_record.children[2].is_root == False)
+    # The "dir1" directory should have an ISO9660 mangled name of "DIR1".
+    assert(iso.pvd.root_dir_record.children[2].file_ident == "DIR1")
+    # The "dir1" directory record should have a length of 38.
+    assert(iso.pvd.root_dir_record.children[2].dr_len == 38)
+
+    # The "foo" file should not have any children.
+    assert(len(iso.pvd.root_dir_record.children[3].children) == 0)
+    # The "foo" file should not be a directory.
+    assert(iso.pvd.root_dir_record.children[3].isdir == False)
+    # The "foo" file should not be the root.
+    assert(iso.pvd.root_dir_record.children[3].is_root == False)
+    # The "foo" file should have an ISO9660 mangled name of "FOO.;1".
+    assert(iso.pvd.root_dir_record.children[3].file_ident == "FOO.;1")
+    # The "foo" directory record should have a length of 40.
+    assert(iso.pvd.root_dir_record.children[3].dr_len == 40)
+    # Make sure getting the data from the foo file works, and returns the right
+    # thing.
+    out = StringIO.StringIO()
+    iso.get_and_write("/foo", out)
+    assert(out.getvalue() == "foo\n")
