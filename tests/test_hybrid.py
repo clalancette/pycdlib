f63a9a1f28bd68330d1fae2e4e650baf877d412e
Chris Lalancette <clalancette@gmail.com>
2015-05-22 21:45:18 -0400

    Add in hybrid tests.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

---
+++ tests/test_hybrid.py
@@ -0,0 +1,37 @@
+import pytest
+import subprocess
+import os
+import sys
+import StringIO
+import struct
+
+prefix = '.'
+for i in range(0,3):
+    if os.path.exists(os.path.join(prefix, 'pyiso.py')):
+        sys.path.insert(0, prefix)
+        break
+    else:
+        prefix = '../' + prefix
+
+import pyiso
+
+from common import *
+
+def test_hybrid_twofiles(tmpdir):
+    # First set things up, and generate the ISO with genisoimage.
+    outfile = tmpdir.join("twofile-test.iso")
+    indir = tmpdir.mkdir("twofile")
+    outfp = open(os.path.join(str(tmpdir), "twofile", "foo"), 'wb')
+    outfp.write("foo\n")
+    outfp.close()
+    subprocess.call(["genisoimage", "-v", "-v", "-iso-level", "1", "-no-pad",
+                     "-o", str(outfile), str(indir)])
+
+    # Now open up the ISO with pyiso and check some things out.
+    iso = pyiso.PyIso()
+    iso.open(open(str(outfile), 'rb'))
+
+    barstr = "bar\n"
+    iso.add_fp(StringIO.StringIO(barstr), len(barstr), "/BAR")
+
+    check_twofile(iso)
