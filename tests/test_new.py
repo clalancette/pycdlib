a0377073389b3fba0f270fe7d6be78faadf09f92
Chris Lalancette <clalancette@gmail.com>
2015-05-28 22:32:20 -0400

    Finish tendirs test.
    We had never added the checks for it.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- tests/test_new.py
+++ tests/test_new.py
@@ -92,7 +92,29 @@ def test_new_tendirs():
     for i in range(1, 1+numdirs):
         iso.add_directory("/DIR%d" % i)
 
+    # Do checks on the PVD.  With ten directories, the ISO should be 35 extents
+    # (24 extents for the metadata, and 1 extent for each of the ten
+    # directories).  The path table should be 132 bytes (10 bytes for the root
+    # directory entry, and 12 bytes for each of the first nine "dir?" records,
+    # and 14 bytes for the last "dir10" record).
+    check_pvd(iso.pvd, 34, 132, 21)
+
     check_terminator(iso.vdsts)
+
+    # Now check the root directory record.  With ten directories at at the root,
+    # the root directory record should have "dot", "dotdot", and the ten
+    # directories as children.
+    check_root_dir_record(iso.pvd.root_dir_record, 12, 2048, 23)
+
+    # Now check the "dot" directory record.
+    check_dot_dir_record(iso.pvd.root_dir_record.children[0])
+
+    # Now check the "dotdot" directory record.
+    check_dotdot_dir_record(iso.pvd.root_dir_record.children[1])
+
+    names = generate_inorder_names(numdirs)
+    for index in range(2, 2+numdirs):
+        check_directory(iso.pvd.root_dir_record.children[index], names[index])
 
 def test_new_manydirs():
     numdirs = 295
