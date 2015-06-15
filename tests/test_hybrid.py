48261c7efe19493269c33aa36c1d9e1bbd366889
Chris Lalancette <clalancette@gmail.com>
2015-06-14 22:16:00 -0400

    Add in a hybrid test removing all entries.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- tests/test_hybrid.py
+++ tests/test_hybrid.py
@@ -163,4 +163,23 @@ def test_hybrid_rmsubdir(tmpdir):
 
     check_onedir(iso, len(out.getvalue()))
 
-# FIXME: add a test to test removing all files and directories
+def test_hybrid_removeall(tmpdir):
+    # First set things up, and generate the ISO with genisoimage.
+    outfile = tmpdir.join("twoleveldeepdir-test.iso")
+    indir = tmpdir.mkdir("twoleveldeep")
+    tmpdir.mkdir(os.path.join("twoleveldeep", "dir1"))
+    tmpdir.mkdir(os.path.join("twoleveldeep", "dir1", "subdir1"))
+    subprocess.call(["genisoimage", "-v", "-v", "-iso-level", "1", "-no-pad",
+                     "-o", str(outfile), str(indir)])
+
+    # Now open up the ISO with pyiso and check some things out.
+    iso = pyiso.PyIso()
+    iso.open(open(str(outfile), 'rb'))
+
+    iso.rm_directory("/DIR1/SUBDIR1")
+    iso.rm_directory("/DIR1")
+
+    out = StringIO.StringIO()
+    iso.write(out)
+
+    check_nofile(iso, len(out.getvalue()))
