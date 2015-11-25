a6343618ba46bd46133301ed8a5af171cc578280
Chris Lalancette <clalancette@gmail.com>
2015-11-25 18:36:27 -0500

    Get the RPM working properly.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,42 @@
 import distutils.core
+from distutils.command.sdist import sdist as _sdist
+import subprocess
+import time
 
 VERSION='0.1.0'
+RELEASE='0'
+
+class sdist(_sdist):
+    """ custom sdist command, to prep pyiso.spec file for inclusion """
+
+    def run(self):
+        global VERSION
+        global RELEASE
+
+        # Create a development release string for later use
+        git_head = subprocess.Popen("git log -1 --pretty=format:%h",
+                                    shell=True,
+                                    stdout=subprocess.PIPE).communicate()[0].strip()
+        date = time.strftime("%Y%m%d%H%M%S", time.gmtime())
+        git_release = "%sgit%s" % (date, git_head)
+
+        # Expand macros in pyiso.spec.in and create pyiso.spec
+        spec_in = open('pyiso.spec.in', 'r')
+        spec = open('pyiso.spec', 'w')
+        for line in spec_in.xreadlines():
+            if "@VERSION@" in line:
+                line = line.replace("@VERSION@", VERSION)
+            elif "@RELEASE@" in line:
+                # If development release, include date+githash in %{release}
+                if RELEASE.startswith('0'):
+                    RELEASE += '.' + git_release
+                line = line.replace("@RELEASE@", RELEASE)
+            spec.write(line)
+        spec_in.close()
+        spec.close()
+
+        # Run parent constructor
+        _sdist.run(self)
 
 distutils.core.setup(name='pyiso',
                      version=VERSION,
@@ -19,5 +55,6 @@ distutils.core.setup(name='pyiso',
                      py_modules=['pyiso'],
                      install_requires=['pysendfile'],
                      package_data={},
+                     cmdclass={'sdist': sdist},
                      data_files=[],
 )
