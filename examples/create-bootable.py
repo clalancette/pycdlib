d4a8424c7bbeb3ee0028f54f77896583804ff732
Chris Lalancette <clalancette@gmail.com>
2016-07-26 20:39:21 -0400

    Add additional examples.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

---
+++ examples/create-bootable.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+# This is program to show how to use PyIso to create a new ISO that is
+# bootable.
+
+# Import standard python modules.
+import sys
+import StringIO
+
+# Import pyiso itself.
+import pyiso
+
+# Check that there are enough command-line arguments.
+if len(sys.argv) != 1:
+    print("Usage: %s" % (sys.argv[0]))
+    sys.exit(1)
+
+# Create a new PyIso object.
+iso = pyiso.PyIso()
+
+# Create a new ISO, accepting all of the defaults.
+iso.new()
+
+# Add a new file to the ISO, with the contents coming from the file object.
+# This file will be used as the boot file on the bootable ISO.
+bootstr = "boot\n"
+iso.add_fp(StringIO.StringIO(bootstr), len(bootstr), '/BOOT.;1')
+
+# Once the bootable file is on the ISO, we need to link it to the boot catalog
+# by calling add_eltorito.
+iso.add_eltorito('/BOOT.;1', bootcatfile='/BOOT.CAT;1')
+
+# Write out the ISO to the file called 'eltorito.iso'.  This will fully master
+# the ISO, making it bootable.
+iso.write('eltorito.iso')
+
+# Close the ISO object.  After this call, the PyIso object has forgotten
+# everything about the previous ISO, and can be re-used.
+iso.close()
---
+++ examples/extract-data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+# This is a simple program to show how to use PyIso to extract data from the
+# ISO.
+
+# Import standard python modules.
+import sys
+import StringIO
+
+# Import pyiso itself.
+import pyiso
+
+# Check that there are enough command-line arguments.
+if len(sys.argv) != 1:
+    print("Usage: %s" % (sys.argv[0]))
+    sys.exit(1)
+
+
+# First we'll create a new ISO and write it out (see create-new.py for more
+# information about these steps).
+iso = pyiso.PyIso()
+iso.new()
+foostr = "foo\n"
+iso.add_fp(StringIO.StringIO(foostr), len(foostr), '/FOO.;1')
+out = StringIO.StringIO()
+iso.write_fp(out)
+iso.close()
+
+# Now, let's open up the ISO and extract the contents of the FOO.;1 file.
+iso.open_fp(out)
+extracted = StringIO.StringIO()
+# Use the get_and_write_fp() API to extract the named filename into the file
+# descriptor.
+iso.get_and_write_fp("/FOO.;1", extracted)
+iso.close()
+
+print(extracted)
