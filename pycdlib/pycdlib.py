9f3a91d13c0865cd9c29c1974ef26cecb71bd906
Chris Lalancette <clalancette@gmail.com>
2017-11-28 20:32:41 -0500

    Make sure to only check path depth for non-rr ISOs.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- pycdlib/pycdlib.py
+++ pycdlib/pycdlib.py
@@ -3356,7 +3356,9 @@ class PyCdlib(object):
 
         joliet_path = self._normalize_joliet_path(joliet_path)
 
-        _check_path_depth(symlink_path)
+        if self.rock_ridge is None and self.enhanced_vd is None:
+            _check_path_depth(iso_path)
+
         (name, parent) = _name_and_parent_from_path(self.pvd, symlink_path)
 
         rec = dr.DirectoryRecord()
