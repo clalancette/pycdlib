0ad203b05a78460e5c8764012d7903f5a9888f96
Chris Lalancette <clalancette@gmail.com>
2018-01-09 20:41:13 -0500

    Remove a test.
    It tests functionality that is now gone.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1373,24 +1373,6 @@ def test_parse_open_invalid_pvd_unused2(tmpdir):
     with pytest.raises(pycdlib.pycdlibexception.PyCdlibInvalidISO):
         iso.open(str(outfile))
 
-def test_parse_open_invalid_pvd_unused3(tmpdir):
-    # First set things up, and generate the ISO with genisoimage.
-    indir = tmpdir.mkdir("modifyinplaceisolevel4onefile")
-    outfile = str(indir)+".iso"
-    subprocess.call(["genisoimage", "-v", "-v", "-iso-level", "4", "-no-pad",
-                     "-o", str(outfile), str(indir)])
-
-    # Now that we've made a valid ISO, we open it up and perturb the first
-    # byte.  This should be enough to make an invalid ISO.
-    with open(str(outfile), 'r+b') as fp:
-        fp.seek((16*2048)+88)
-        fp.write(b'\x02')
-
-    iso = pycdlib.PyCdlib()
-
-    with pytest.raises(pycdlib.pycdlibexception.PyCdlibInvalidISO):
-        iso.open(str(outfile))
-
 def test_parse_open_invalid_pvd_file_structure_version(tmpdir):
     # First set things up, and generate the ISO with genisoimage.
     indir = tmpdir.mkdir("modifyinplaceisolevel4onefile")
