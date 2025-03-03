fbf4666775debe49885d6272c033bd4033c9b418
Chris Lalancette <clalancette@gmail.com>
2025-03-02 20:34:24 -0500

    Fix typo.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- pycdlib/__init__.py
+++ pycdlib/__init__.py
@@ -2,4 +2,4 @@
 Pycdlib is a pure python library to parse, write (master), and create ISO9660
 files.  These files are suitable for writing to a CD or USB.
 """
-from .pycdlib import PyCdlib as PyCdlib  # NOAQ, pylint: disable=useless-import-alias
+from .pycdlib import PyCdlib as PyCdlib  # NOQA, pylint: disable=useless-import-alias
